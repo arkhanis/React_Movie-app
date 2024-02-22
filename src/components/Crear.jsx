31d1da178410d5a9e854f1cfc14db4c8d1d98cb6
arkhanis <casanovac@gmail.com>
2024-02-22 11:39:07 -0300

    Create components

--- src/components/Crear.jsx
+++ src/components/Crear.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useState } from "react"
 
 const Crear = () => {
+
+    const tituloComponente = "Añadir Pelicula";
+
+    const [peliState, setPeliState] = useState({
+        titulo: '',
+        descripcion: ''
+    });
+
+    const { titulo, descripcion } = peliState;
+
+    const conseguirDatosForm = (e) => {
+        e.preventDefault();
+
+        // Conseguir los datos del formulario:
+        let target = e.target;
+        let titulo = target.titulo.value;
+        let descripcion = target.descripcion.value;
+
+        // Crear objeto de la pelicula a guardar:
+        let peli = {
+            id: new Date().getTime(),
+            titulo,
+            descripcion
+        }
+
+        setPeliState(peli);
+    }
+
     return (
         <>
             <div className="add">
-                <h3 className="title">Añadir pelicula</h3>
-                <form>
-                    <input type="text" id="title" placeholder="Titulo" />
-                    <textarea id="description" placeholder="Descripción"></textarea>
-                    <input type="submit" id="save" value="Guardar" />
+                <h3 className="title">{tituloComponente}</h3>
+
+                <strong>
+                    {(titulo && descripcion) && "Haz creado la pelicula: " + titulo}
+                </strong>
+
+                <form onSubmit={conseguirDatosForm}>
+                    <input
+                        type="text"
+                        id="title"
+                        name='titulo'
+                        placeholder="Titulo"
+                    />
+                    <textarea
+                        id="descripcion"
+                        name='descripcion'
+                        placeholder="Descripción"
+                    >
+                    </textarea>
+                    <input
+                        type="submit"
+                        id="save"
+                        value="Guardar"
+                    />
                 </form>
             </div>
         </>
