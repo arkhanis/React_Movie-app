502bad5e9e777674d71023a2a284d5bac6a6a0d6
arkhanis <casanovac@gmail.com>
2024-02-22 10:22:50 -0300

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Listado from './components/Listado'
+import Buscador from './components/Buscador'
+import Crear from './components/Crear'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1>Movies</h1>
+      <div className="layout">
+        <header className="header">
+          <div className="logo">
+            <div className="play"></div>
+          </div>
+
+          <h1>MisPelis</h1>
+        </header>
+
+        <nav className="nav">
+          <ul>
+            <li><a href="/#">Inicio</a></li>
+            <li><a href="/#">Peliculas</a></li>
+            <li><a href="/#">Blog</a></li>
+            <li><a href="/#">Contacto</a></li>
+          </ul>
+        </nav>
+
+        {/* Listado de peliculas */}
+        <section id="content" className="content">
+          <Listado />
+        </section>
+
+        <aside className="lateral">
+          <Buscador />
+
+          <Crear />
+        </aside>
+
+        <footer className="footer">
+          &copy; Cesar Casanova - <a href="https://casanovacesar.com">casanovacesar.com</a>
+        </footer>
+
+      </div>
     </>
   )
 }
