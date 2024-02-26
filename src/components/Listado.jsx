cd5ae4009f7ac93e845f7871df717940c2a0e1d4
arkhanis <casanovac@gmail.com>
2024-02-26 11:19:00 -0300

    component Listado

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,41 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Listado = () => {
+
+    // Hooks
+    const [listadoState, SetListadoState] = useState([]);
+
+    useEffect(() => {
+        console.log('Componente de listado de pelicula cargado!');
+        conseguirPeliculas();
+    }, []);
+
+    // Funciones
+    const conseguirPeliculas = () => {
+        let peliculas = JSON.parse(localStorage.getItem("pelis"))
+        SetListadoState(peliculas);
+    }
+
+
     return (
         <>
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
+            {/* primero verifico que el listado no este vacio y luego recorro */}
+            {listadoState != null ? 
+            
+                    listadoState.map(peli => {
+                        return (
+                            <article key={peli.id} className="peli-item">
+                                <h3 className="title">{peli.titulo}</h3>
+                                <p className="description">{peli.descripcion}</p>
 
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+                                <button className="edit">Editar</button>
+                                <button className="delete">Borrar</button>
+                            </article>
+                        );
+                    })
 
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+                    :
+                    <h2>No hay peliculas cargadas!</h2>
+                }
         </>
-    )
+    );
+
 }
 
 export default Listado
