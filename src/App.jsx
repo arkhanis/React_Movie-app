f742ba86c0b952d44f3d596c87ecc126cf1ce9f3
arkhanis <casanovac@gmail.com>
2024-02-28 11:29:39 -0300

    Feature - Buscador

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,10 @@ function App() {
         </section>
 
         <aside className="lateral">
-          <Buscador />
+          <Buscador 
+            listadoState={listadoState}
+            setListadoState={setListadoState}
+          />
 
           <Crear 
             setListadoState={setListadoState}
